75341555876937f26cbf57a87a3846b262b81b6c
Jus de Patate_ <jusdepatate@protonmail.com>
2020-04-15 11:05:59 +0200

    Random String 64 Bytes + Ping Example

---
+++ RandomString64Bytes.cs/RandomString64Bytes.cs
@@ -0,0 +1,18 @@
+        public static string RandomString64Bytes()
+        {
+            string finalString = "";
+
+            int i = 0;
+            while (i <= 8) 
+            {
+                string randomString = Path.GetRandomFileName();
+                randomString = randomString.Replace(".", "");
+                randomString = randomString.Substring(0, 8);
+
+                finalString += randomString;
+                
+                i += 1;
+            }
+            
+            return finalString;
+        }
